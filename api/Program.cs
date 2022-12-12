03ca011d3e7ca1e4ee3d06a5f3bbd1af1e1395d7
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2022-12-12 07:07:55 -0300

    adaptando ao minimal api

--- api/Program.cs
+++ api/Program.cs
@@ -1,17 +1,12 @@
 
-namespace MinimalApiDesafio;
+using MinimalApiDesafio;
 
-public class Program
-{
-    public static void Main(string[] args)
+IHostBuilder CreateHostBuilder(string[] args){
+  return Host.CreateDefaultBuilder(args)
+    .ConfigureWebHostDefaults(webBuilder =>
     {
-        CreateHostBuilder(args).Build().Run();
-    }
-
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-                webBuilder.UseStartup<Startup>();
-            });
+        webBuilder.UseStartup<Startup>();
+    });
 }
+
+CreateHostBuilder(args).Build().Run();
