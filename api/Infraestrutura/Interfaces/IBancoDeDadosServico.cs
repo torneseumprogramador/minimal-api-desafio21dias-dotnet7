3d8130a9319e56a47aa9cb1fd8a42b1819f47eed
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2022-12-15 07:47:53 -0300

    testes finalizados

--- api/Infraestrutura/Interfaces/IBancoDeDadosServico.cs
+++ api/Infraestrutura/Interfaces/IBancoDeDadosServico.cs
@@ -5,6 +5,6 @@ public interface IBancoDeDadosServico<T>
     Task Salvar(T objeto);
     Task Excluir(T objeto);
     Task ExcluirPorId(int id);
-    Task<T> BuscaPorId(int id);
+    Task<T?> BuscaPorId(int id);
     Task<List<T>> Todos();
 }
