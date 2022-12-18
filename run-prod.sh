2a2740dc2e24b97c2e8d5eb1c8a7040386de44cf
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2022-12-18 07:01:18 -0300

    Update run-prod.sh

--- run-prod.sh
+++ run-prod.sh
@@ -1,10 +1,8 @@
 cd api
 echo "====[Rodando migração prod]===="
-#DATABASE_URL_MINIMAL_API="Server=localhost;Database=desafio21dias_dotnet7_prod;Uid=root;Pwd=root" dotnet ef database update # create base de dev ou prod
+DATABASE_URL_MINIMAL_API="Server=localhost;Database=desafio21dias_dotnet7_prod;Uid=root;Pwd=root" dotnet ef database update # create base de dev ou prod
 
 dotnet publish -o Release
 cp appsettings.Sample.json appsettings.json
 export DATABASE_URL_MINIMAL_API="Server=localhost;Database=desafio21dias_dotnet7_prod;Uid=root;Pwd=root"
-#nohup dotnet Release/minimal-api-desafio.dll &
-
-dotnet Release/minimal-api-desafio.dll
+nohup dotnet Release/minimal-api-desafio.dll &
