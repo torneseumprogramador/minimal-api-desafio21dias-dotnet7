1f484ae3245c7ae10e9b27edbb32da8e08ad086a
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2022-12-18 06:56:48 -0300

    Update run-prod.sh

--- run-prod.sh
+++ run-prod.sh
@@ -5,4 +5,6 @@ DATABASE_URL_MINIMAL_API="Server=localhost;Database=desafio21dias_dotnet7_prod;U
 dotnet publish -o Release
 cp appsettings.Sample.json appsettings.json
 export DATABASE_URL_MINIMAL_API="Server=localhost;Database=desafio21dias_dotnet7_prod;Uid=root;Pwd=root"
-nohup dotnet Release/minimal-api-desafio.dll &
+#nohup dotnet Release/minimal-api-desafio.dll &
+
+dotnet Release/minimal-api-desafio.dll
